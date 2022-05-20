4683fce75635ec977f400b8be4ab6e1d53a8ce2c
Noah <noa316@naver.com>
2022-05-20 17:36:44 +0900

    Initialize Project

---
+++ MOZI/MOZI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MOZI
+//
+//  Created by Noah on 2022/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
