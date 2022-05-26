db192121be8b95560658dfe6e3e401f43ea385dd
Noah <noa316@naver.com>
2022-05-26 22:26:33 +0900

    [Feat] DefaultTabBarItemLiteral 정의

--- MOZI/MOZI/Util/Literal/TabBarItemLiteral.swift
+++ MOZI/MOZI/Util/Literal/TabBarItemLiteral.swift
@@ -5,4 +5,38 @@
 //  Created by Noah on 2022/05/25.
 //
 
-import Foundation
+import UIKit
+
+enum DefaultTabBarItem: CaseIterable {
+    case home
+    case friend
+    case community
+    case setting
+    
+    var index: Int {
+        switch self {
+        case .home: return 0
+        case .friend: return 1
+        case .community: return 2
+        case .setting: return 3
+        }
+    }
+    
+    var deSelectedImage: UIImage {
+        switch self {
+        case .home: return .load(named: .iconHomeDeSelected)
+        case .friend: return .load(named: .iconFriendDeSelected)
+        case .community: return .load(named: .iconCommunityDeSelected)
+        case .setting: return .load(named: .iconSettingDeSelected)
+        }
+    }
+    
+    var selectedImage: UIImage {
+        switch self {
+        case .home: return .load(named: .iconHomeSelected)
+        case .friend: return .load(named: .iconFriendSelected)
+        case .community: return .load(named: .iconCommunitySelected)
+        case .setting: return .load(named: .iconSettingSelected)
+        }
+    }
+}
